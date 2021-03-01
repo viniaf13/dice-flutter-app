93c7f2047b6052bf78be8c184c0647c454883342
Vinicius Aguiar <viniaf13@gmail.com>
2021-03-01 14:19:34 -0300

    finished basic dice app

--- lib/main.dart
+++ lib/main.dart
@@ -56,39 +56,8 @@ class _DicePageState extends State<DicePage> {
   }
 }
 
-class DicePage2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    int leftDiceNumber = 1;
-    int rightDiceNumber = 1;
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                leftDiceNumber = rollDice();
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                rightDiceNumber = rollDice();
-              },
-              child: Image.asset(('images/dice$rightDiceNumber.png')),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 int rollDice() {
   var rnd = Random();
   int result = 1 + rnd.nextInt(6);
-  print(result);
   return result;
 }
