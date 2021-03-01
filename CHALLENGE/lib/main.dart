a2824c4790877abe79974dd24e9cb6ea1781e5b6
Vinicius Aguiar <viniaf13@gmail.com>
2021-03-01 14:57:48 -0300

    Finished app challenge

--- CHALLENGE/lib/main.dart
+++ CHALLENGE/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -9,6 +11,45 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Center(child: Text('Ask Me Anything')),
+        ),
+        body: Ball());
   }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballState = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballState = answerRandomizer();
+                });
+              },
+              child: Image.asset('images/ball$ballState.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+int answerRandomizer() {
+  int result = Random().nextInt(5) + 1;
+  return result;
 }
