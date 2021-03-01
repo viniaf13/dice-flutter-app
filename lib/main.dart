391adbb3b7875af2e45a9b0e6f6502ec39b5c191
Vinicius Aguiar <viniaf13@gmail.com>
2021-03-01 13:51:26 -0300

    added button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[300],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.blue[400],
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,78 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = rollDice();
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = rollDice();
+                });
+              },
+              child: Image.asset(('images/dice$rightDiceNumber.png')),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class DicePage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int leftDiceNumber = 1;
+    int rightDiceNumber = 1;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                leftDiceNumber = rollDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rightDiceNumber = rollDice();
+              },
+              child: Image.asset(('images/dice$rightDiceNumber.png')),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+int rollDice() {
+  var rnd = Random();
+  int result = 1 + rnd.nextInt(6);
+  print(result);
+  return result;
 }
